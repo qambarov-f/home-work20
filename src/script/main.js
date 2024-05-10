082117c5d5412375e7f8d08c883fe0628f2abaf4
qambarov-f <qemberov.f1949@gmail.com>
2024-05-10 10:09:09 +0400

    i did homework 1 2

---
+++ src/script/main.js
@@ -0,0 +1,44 @@
+// ! homework 1
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 1) {
+//         arr[i] = arr[i] + 1;
+//         console.log(arr[i]);
+//     }
+// }
+
+// ! homework 2
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     console.log(arr[i]);
+//     // arr[i] = arr[i] * arr[0];
+//     // console.log(arr[i]);
+//   } else {
+//     // arr[i] = arr[0] * arr[i];
+//     // console.log("---", arr[i]);
+//   }
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     arr[i] *= arr[arr.length - 1];
+//   } else {
+//     arr[i] *= arr[0];
+//   }
+// }
+// console.log(arr);
+// console.log(arr[9]);
+
+// ! homework 3
+// 3. reqemlerden ibaret bir array yaradin ve icinde gezerek arrayin en boyuk ve en kicik elementini tapin daha sonra tapdiginiz bu iki reqemin hasilini console-a yazdirin.
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+
+// }
